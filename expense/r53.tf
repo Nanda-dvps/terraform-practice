e93083162f1550ed582b63b68e619ca0bda521a4
Nanda-dvps <pnr956244@gmail.com>
2024-09-21 03:35:51 +0100

    terraform

--- expense/r53.tf
+++ expense/r53.tf
@@ -1,7 +1,6 @@
 resource "aws_route53_record" "expense" {
   count = length(var.instance_names)
   zone_id = var.zone_id
-  #backend.daws81s.online
   name    = "${var.instance_names[count.index]}.${var.domain_name}"
   type    = "A"
   ttl     = 1
